b0daa81c8525c1647f8878023c2c0cc34b885f08
marcel_voets@hotmail.com <marcel_voets@hotmail.com@94b9075c-ca49-11de-8bd7-25a952cf5249>
2009-11-19 23:03:28 +0000

    Nu werkt de autocomplete op zijn lichts en op elke database-server als de database maar op de 'SQLEXPRESS' instantie staat

--- trunk/SimpleTalk/Model/AutoComplete.cs
+++ trunk/SimpleTalk/Model/AutoComplete.cs
@@ -1,6 +1,4 @@
-﻿#define DATABASE_ENABLED //uncomment this line to enable autocompletion database
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,20 +22,22 @@ namespace SimpleTalk.Model
       }
     }
 
-#if (DATABASE_ENABLED)
     private DataAccess.AutoWords _autoWords;
     private DataAccess.NextWords _nextWords;
-#endif
-
 
     public AutoComplete()
     {
-#if (DATABASE_ENABLED)
-      DataAccess.Database.Connect();
+      try
+      {
+        DataAccess.Database.Connect();
 
-      _autoWords = new DataAccess.AutoWords();
-      _nextWords = new DataAccess.NextWords();
-#endif
+        _autoWords = new DataAccess.AutoWords();
+        _nextWords = new DataAccess.NextWords();
+      }
+      catch //(Exception ex)
+      {
+        //System.Windows.Forms.MessageBox.Show(ex.Message, "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+      }
     }
 
     public List<string> Suggestions
@@ -48,194 +48,208 @@ namespace SimpleTalk.Model
       }
     }
 
-    public void ImportWords(string fileNamePath)
+    public void ImportWords(string fileNamePath, int countOffset, int countOffsetWhenDouble)
     {
-      string[] wordArray = File.ReadAllLines(fileNamePath);
-
-      foreach (string word in wordArray)
+      try
       {
-        // Todo:  make database connection and add word row to database
-        // Todo: if word row does exists skip word
+        string[] wordArray = File.ReadAllLines(fileNamePath);
 
-#if (DATABASE_ENABLED)
-        _autoWords.Add(new Model.AutoWord(default(int), word, 0, false, false));
-#endif
+        Model.AutoWord autoWord = null;
 
+        foreach (string word in wordArray)
+        {
+          autoWord = _autoWords.GetByWord(word);
+
+          if (autoWord == null)
+          {
+            _autoWords.Add(new Model.AutoWord(default(int), word, countOffset, false, false));
+          }
+          else
+          {
+            autoWord.Word = autoWord.Word.ToLower();
+            autoWord.Count = countOffsetWhenDouble;
+
+            _autoWords.Modify(autoWord);
+          }
+        }
+      }
+      catch //(Exception ex)
+      {
+        //System.Windows.Forms.MessageBox.Show(ex.Message, "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
       }
     }
 
     private void SetWordsCount(string word)
     {
-      //Todo: make database connection and update word counter.
-      //Todo: if word row does not exists create row
-
-#if (DATABASE_ENABLED)
-      _autoWords.UpdateWordCount(word);
-#endif
-
-      //for debugging:
-      //_listSuggestions.Add("SetWordsCount(" + word + ")");
+      try
+      {
+        _autoWords.UpdateWordCount(word);
+      }
+      catch //(Exception ex)
+      {
+        //System.Windows.Forms.MessageBox.Show(ex.Message, "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+      }
     }
 
     private void SetNextWordsCount(string secondLastWord, string lastWord)
     {
-      //Todo: make database connection and update next word counter.
-      //Todo: if nextWord row does not exists create it
-
-#if (DATABASE_ENABLED)
-      _nextWords.UpdateNextWordCount(secondLastWord, lastWord);
-#endif
-
-      //for debugging:
-      //_listSuggestions.Add("SetNextWordsCount(" + secondLastWord + "," + lastWord + ")");
+      try
+      {
+        _nextWords.UpdateNextWordCount(secondLastWord, lastWord);
+      }
+      catch //(Exception ex)
+      {
+        //System.Windows.Forms.MessageBox.Show(ex.Message, "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+      }
     }
 
     private void GetWordList(string word)
     {
-      //Todo: make database connection and return word list.
+      try
+      {
+        _Suggestions = _autoWords.GetWordList(word);
 
-#if (DATABASE_ENABLED)
-      _Suggestions = _autoWords.GetWordList(word);
-#endif
-
-      //for debugging:
-      _Suggestions.Add(word);
-
-      OnSuggestionsChanged(this, new EventArgs());
+        OnSuggestionsChanged(this, new EventArgs());
+      }
+      catch //(Exception ex)
+      {
+        //System.Windows.Forms.MessageBox.Show(ex.Message, "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+      }
     }
 
     private void GetNextWordList(string word)
     {
-      
-#if (DATABASE_ENABLED)
-      _Suggestions = _nextWords.GetNextWordList(word);
-#endif
-
-      //Gives only next word suggestions when there are 3 or more options in the database to make it hard for regenerate the last typed sentences. (Privacy issue)
-      if (_Suggestions.Count() < 3)
+      try
       {
-        _Suggestions.Clear();
+        _Suggestions = _nextWords.GetNextWordList(word);
+
+        if (_Suggestions.Count < 3)
+        {
+          _Suggestions.Clear();
+        }
+
+        OnSuggestionsChanged(this, new EventArgs());
       }
-
-
-      //for debugging:
-      //_listSuggestions.Add("GetNextWord(" + word + ")");
-
-      OnSuggestionsChanged(this, new EventArgs());
+      catch //(Exception ex)
+      {
+        //System.Windows.Forms.MessageBox.Show(ex.Message, "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+      }
     }
 
     public void OnTextChanged(string text)
     {
-      //note that C# adds "\r\n" to the end of a line. 
-      //"\r\n" should also be added when a on screen buton "carriage return" is implemented
-
-      if (string.IsNullOrEmpty(text))
+      try
       {
-        textLength = 0;
-      }
-      else
-      {
-        textLength = text.Length;
-      }
-      //for debugging:
-      //_listSuggestions.Clear();
+        //note that C# adds "\r\n" to the end of a line. 
+        //"\r\n" should also be added when a on screen buton "carriage return" is implemented
 
-      if (string.IsNullOrEmpty(text))
-      {
-        //return empty list in case of empty string
-        Suggestions.Clear();  //This line should be uncommented in the final code 
-        OnSuggestionsChanged(this, new EventArgs());
-
-        //for debugging:
-        //_listSuggestions.Add("Clear list");
-      }
-      else
-      {
-        List<string> lineArray = new List<string>(text.Split(new string[] { "\r\n" }, StringSplitOptions.None));
-        List<string> wordArray = new List<string>();
-
-        if (lineArray != null && lineArray.Count > 0)//if is required for handling empty list exception
+        if (string.IsNullOrEmpty(text))
         {
-          foreach (string word in lineArray[lineArray.Count - 1].Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries))
-          {
-            wordArray.Add(word);
-          }
-        }
-
-        if (text[text.Length - 1].Equals(' '))
-        {
-          //Last word is completed (with space)
-
-          //Count only words if text size is increasing to prevent double counting when line is cleared with backspace
-          if (textLength > previousTextLength && wordArray.Count > 0 && text.Length >= 2)
-          {
-            //filter for two successive split comments to prevent double word counting
-            //note that C# adds "\r\n" to the end of a line.
-            if (!text[text.Length - 2].Equals(' ') && !text[text.Length - 2].Equals('\n'))
-            {
-              SetWordsCount(wordArray[wordArray.Count - 1]);
-
-              if (wordArray.Count >= 2)
-              {
-                //Two or more complete words
-                SetNextWordsCount(wordArray[wordArray.Count - 2], wordArray[wordArray.Count - 1]);
-              }
-            }
-          }
+          textLength = 0;
         }
         else
         {
-          if (text[text.Length - 1].Equals('\n'))
+          textLength = text.Length;
+        }
+
+        if (string.IsNullOrEmpty(text))
+        {
+          //return empty list in case of empty string
+          Suggestions.Clear();  //This line should be uncommented in the final code 
+          OnSuggestionsChanged(this, new EventArgs());
+        }
+        else
+        {
+          List<string> lineArray = new List<string>(text.Split(new string[] { "\r\n" }, StringSplitOptions.None));
+          List<string> wordArray = new List<string>();
+
+          if (lineArray != null && lineArray.Count > 0)//if is required for handling empty list exception
           {
-            //Last word is completed (with enter)
+            foreach (string word in lineArray[lineArray.Count - 1].Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries))
+            {
+              wordArray.Add(word);
+            }
+          }
+
+          if (text[text.Length - 1].Equals(' '))
+          {
+            //Last word is completed (with space)
 
             //Count only words if text size is increasing to prevent double counting when line is cleared with backspace
-            if (textLength > previousTextLength && text.Length >= 3)
+            if (textLength > previousTextLength && wordArray.Count > 0 && text.Length >= 2)
             {
               //filter for two successive split comments to prevent double word counting
               //note that C# adds "\r\n" to the end of a line.
-              if (!text[text.Length - 3].Equals(' ') && !text[text.Length - 3].Equals('\n'))
+              if (!text[text.Length - 2].Equals(' ') && !text[text.Length - 2].Equals('\n'))
               {
-                //Get words from previous line
-                if (lineArray != null && lineArray.Count > 1)//if is required for handling empty list exception
-                {
-                  wordArray.Clear();
-                  foreach (string word in lineArray[lineArray.Count - 2].Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries))
-                  {
-                    wordArray.Add(word);
-                  }
-                }
-                if (wordArray.Count > 0)
-                {
-                  SetWordsCount(wordArray[wordArray.Count - 1]);
+                SetWordsCount(wordArray[wordArray.Count - 1]);
 
-                  if (wordArray.Count >= 2)
+                if (wordArray.Count >= 2)
+                {
+                  //Two or more complete words
+                  SetNextWordsCount(wordArray[wordArray.Count - 2], wordArray[wordArray.Count - 1]);
+                }
+              }
+            }
+          }
+          else
+          {
+            if (text[text.Length - 1].Equals('\n'))
+            {
+              //Last word is completed (with enter)
+
+              //Count only words if text size is increasing to prevent double counting when line is cleared with backspace
+              if (textLength > previousTextLength && text.Length >= 3)
+              {
+                //filter for two successive split comments to prevent double word counting
+                //note that C# adds "\r\n" to the end of a line.
+                if (!text[text.Length - 3].Equals(' ') && !text[text.Length - 3].Equals('\n'))
+                {
+                  //Get words from previous line
+                  if (lineArray != null && lineArray.Count > 1)//if is required for handling empty list exception
                   {
-                    //Two or more complete words
-                    SetNextWordsCount(wordArray[wordArray.Count - 2], wordArray[wordArray.Count - 1]);
+                    wordArray.Clear();
+                    foreach (string word in lineArray[lineArray.Count - 2].Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                      wordArray.Add(word);
+                    }
+                  }
+                  if (wordArray.Count > 0)
+                  {
+                    SetWordsCount(wordArray[wordArray.Count - 1]);
+
+                    if (wordArray.Count >= 2)
+                    {
+                      //Two or more complete words
+                      SetNextWordsCount(wordArray[wordArray.Count - 2], wordArray[wordArray.Count - 1]);
+                    }
                   }
                 }
               }
             }
           }
-        }
 
-        if (text[text.Length - 1].Equals(' ') && wordArray.Count > 0)
-        {
-          //Get suggestions for next word only is space is added and there is a word on the last line
-          GetNextWordList(wordArray[wordArray.Count - 1]);
-        }
-        else
-        {
-          if (wordArray.Count > 0 && !text[text.Length - 1].Equals('\n'))
+          if (text[text.Length - 1].Equals(' ') && wordArray.Count > 0)
           {
-            //Incomplete last word (do not count it)
-            GetWordList(wordArray[wordArray.Count - 1]);
+            //Get suggestions for next word only is space is added and there is a word on the last line
+            GetNextWordList(wordArray[wordArray.Count - 1]);
+          }
+          else
+          {
+            if (wordArray.Count > 0 && !text[text.Length - 1].Equals('\n'))
+            {
+              //Incomplete last word (do not count it)
+              GetWordList(wordArray[wordArray.Count - 1]);
+            }
           }
         }
+        //remember text length
+        previousTextLength = textLength;
       }
-      //remember text length
-      previousTextLength = textLength;
+      catch //(Exception ex)
+      {
+        //System.Windows.Forms.MessageBox.Show(ex.Message, "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+      }
     }
 
     public List<string> GetAutoCompleteList()
@@ -243,15 +257,43 @@ namespace SimpleTalk.Model
       return Suggestions;
     }
 
-    public void Reset()
+    public void Dispose()
     {
-      //Todo: add database reset function
-#if (DATABASE_ENABLED)
-      _nextWords.Reset();
-      _autoWords.Reset();
-#endif
+      try
+      {
+        _nextWords.Dispose();
+        _autoWords.Dispose();
+      }
+      catch //(Exception ex)
+      {
+        //System.Windows.Forms.MessageBox.Show(ex.Message, "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+      }
+    }
 
-      //throw new NotImplementedException();
+    public void ResetDatabase()
+    {
+      try
+      {
+        _nextWords.Reset();
+        _autoWords.Reset();
+      }
+      catch //(Exception ex)
+      {
+        //System.Windows.Forms.MessageBox.Show(ex.Message, "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+      }
+    }
+
+    public void ClearDatabase()
+    {
+      try
+      {
+        _nextWords.Reset();
+        _autoWords.Clear();
+      }
+      catch //(Exception ex)
+      {
+        //System.Windows.Forms.MessageBox.Show(ex.Message, "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+      }
     }
   }
 }
