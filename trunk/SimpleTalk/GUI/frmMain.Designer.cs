4982d03d27c6a4c0e0527e1d6f46720b9ec5f6af
anton@adriessen.nl <anton@adriessen.nl@94b9075c-ca49-11de-8bd7-25a952cf5249>
2009-11-25 20:08:01 +0000

    * Increased txtOutput height so two lines are visible in stead of one (was now the case, caused by increasing the text size) * Increased the maximum auto word suggestion length to fit with the new button width * Fixed null exception add auto words in interpreter (can only occur wen no word is typed but there is still an autoword suggestion)

--- trunk/SimpleTalk/GUI/frmMain.Designer.cs
+++ trunk/SimpleTalk/GUI/frmMain.Designer.cs
@@ -30,102 +30,102 @@ namespace SimpleTalk.GUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.txtOutput = new System.Windows.Forms.TextBox();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.pnlKeyboard = new System.Windows.Forms.Panel();
-            this.pnlAutoComplete = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.Panel2.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.txtOutput);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1284, 157);
-            this.panel1.TabIndex = 11;
-            // 
-            // txtOutput
-            // 
-            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtOutput.BackColor = System.Drawing.SystemColors.Window;
-            this.txtOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtOutput.Location = new System.Drawing.Point(12, 12);
-            this.txtOutput.Multiline = true;
-            this.txtOutput.Name = "txtOutput";
-            this.txtOutput.ReadOnly = true;
-            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtOutput.ShortcutsEnabled = false;
-            this.txtOutput.Size = new System.Drawing.Size(1260, 137);
-            this.txtOutput.TabIndex = 7;
-            this.txtOutput.TabStop = false;
-            this.txtOutput.TextChanged += new System.EventHandler(this.txtOutput_TextChanged);
-            // 
-            // splitContainer1
-            // 
-            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
-            this.splitContainer1.IsSplitterFixed = true;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 163);
-            this.splitContainer1.Name = "splitContainer1";
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.Controls.Add(this.pnlKeyboard);
-            // 
-            // splitContainer1.Panel2
-            // 
-            this.splitContainer1.Panel2.Controls.Add(this.pnlAutoComplete);
-            this.splitContainer1.Size = new System.Drawing.Size(1284, 572);
-            this.splitContainer1.SplitterDistance = 584;
-            this.splitContainer1.TabIndex = 12;
-            // 
-            // pnlKeyboard
-            // 
-            this.pnlKeyboard.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.pnlKeyboard.Location = new System.Drawing.Point(12, 3);
-            this.pnlKeyboard.Name = "pnlKeyboard";
-            this.pnlKeyboard.Size = new System.Drawing.Size(569, 566);
-            this.pnlKeyboard.TabIndex = 6;
-            // 
-            // pnlAutoComplete
-            // 
-            this.pnlAutoComplete.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.pnlAutoComplete.Location = new System.Drawing.Point(3, 3);
-            this.pnlAutoComplete.Name = "pnlAutoComplete";
-            this.pnlAutoComplete.Size = new System.Drawing.Size(681, 566);
-            this.pnlAutoComplete.TabIndex = 11;
-            // 
-            // frmMain
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(1284, 734);
-            this.Controls.Add(this.splitContainer1);
-            this.Controls.Add(this.panel1);
-            this.Name = "frmMain";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_FormClosed);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel2.ResumeLayout(false);
-            this.splitContainer1.ResumeLayout(false);
-            this.ResumeLayout(false);
+          this.panel1 = new System.Windows.Forms.Panel();
+          this.txtOutput = new System.Windows.Forms.TextBox();
+          this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+          this.pnlKeyboard = new System.Windows.Forms.Panel();
+          this.pnlAutoComplete = new System.Windows.Forms.Panel();
+          this.panel1.SuspendLayout();
+          this.splitContainer1.Panel1.SuspendLayout();
+          this.splitContainer1.Panel2.SuspendLayout();
+          this.splitContainer1.SuspendLayout();
+          this.SuspendLayout();
+          // 
+          // panel1
+          // 
+          this.panel1.Controls.Add(this.txtOutput);
+          this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+          this.panel1.Location = new System.Drawing.Point(0, 0);
+          this.panel1.Name = "panel1";
+          this.panel1.Size = new System.Drawing.Size(1284, 157);
+          this.panel1.TabIndex = 11;
+          // 
+          // txtOutput
+          // 
+          this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                      | System.Windows.Forms.AnchorStyles.Left)
+                      | System.Windows.Forms.AnchorStyles.Right)));
+          this.txtOutput.BackColor = System.Drawing.SystemColors.Window;
+          this.txtOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+          this.txtOutput.Location = new System.Drawing.Point(12, 3);
+          this.txtOutput.Multiline = true;
+          this.txtOutput.Name = "txtOutput";
+          this.txtOutput.ReadOnly = true;
+          this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+          this.txtOutput.ShortcutsEnabled = false;
+          this.txtOutput.Size = new System.Drawing.Size(1260, 154);
+          this.txtOutput.TabIndex = 7;
+          this.txtOutput.TabStop = false;
+          this.txtOutput.TextChanged += new System.EventHandler(this.txtOutput_TextChanged);
+          // 
+          // splitContainer1
+          // 
+          this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                      | System.Windows.Forms.AnchorStyles.Left)
+                      | System.Windows.Forms.AnchorStyles.Right)));
+          this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+          this.splitContainer1.IsSplitterFixed = true;
+          this.splitContainer1.Location = new System.Drawing.Point(0, 163);
+          this.splitContainer1.Name = "splitContainer1";
+          // 
+          // splitContainer1.Panel1
+          // 
+          this.splitContainer1.Panel1.Controls.Add(this.pnlKeyboard);
+          // 
+          // splitContainer1.Panel2
+          // 
+          this.splitContainer1.Panel2.Controls.Add(this.pnlAutoComplete);
+          this.splitContainer1.Size = new System.Drawing.Size(1284, 572);
+          this.splitContainer1.SplitterDistance = 584;
+          this.splitContainer1.TabIndex = 12;
+          // 
+          // pnlKeyboard
+          // 
+          this.pnlKeyboard.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                      | System.Windows.Forms.AnchorStyles.Left)
+                      | System.Windows.Forms.AnchorStyles.Right)));
+          this.pnlKeyboard.Location = new System.Drawing.Point(12, 3);
+          this.pnlKeyboard.Name = "pnlKeyboard";
+          this.pnlKeyboard.Size = new System.Drawing.Size(569, 566);
+          this.pnlKeyboard.TabIndex = 6;
+          // 
+          // pnlAutoComplete
+          // 
+          this.pnlAutoComplete.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                      | System.Windows.Forms.AnchorStyles.Left)
+                      | System.Windows.Forms.AnchorStyles.Right)));
+          this.pnlAutoComplete.Location = new System.Drawing.Point(3, 3);
+          this.pnlAutoComplete.Name = "pnlAutoComplete";
+          this.pnlAutoComplete.Size = new System.Drawing.Size(681, 566);
+          this.pnlAutoComplete.TabIndex = 11;
+          // 
+          // frmMain
+          // 
+          this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+          this.BackColor = System.Drawing.SystemColors.Window;
+          this.ClientSize = new System.Drawing.Size(1284, 734);
+          this.Controls.Add(this.splitContainer1);
+          this.Controls.Add(this.panel1);
+          this.Name = "frmMain";
+          this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+          this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_FormClosed);
+          this.panel1.ResumeLayout(false);
+          this.panel1.PerformLayout();
+          this.splitContainer1.Panel1.ResumeLayout(false);
+          this.splitContainer1.Panel2.ResumeLayout(false);
+          this.splitContainer1.ResumeLayout(false);
+          this.ResumeLayout(false);
 
         }
 
