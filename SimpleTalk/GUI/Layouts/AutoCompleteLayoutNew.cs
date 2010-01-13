538133267b5d1bf41a354723cde348567e7bc35d
anton@adriessen.nl <anton@adriessen.nl@94b9075c-ca49-11de-8bd7-25a952cf5249>
2010-01-13 20:27:27 +0000

    * placed auto complete word suggestions on one line

--- SimpleTalk/GUI/Layouts/AutoCompleteLayoutNew.cs
+++ SimpleTalk/GUI/Layouts/AutoCompleteLayoutNew.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
+using System.Windows.Forms;
 
 namespace SimpleTalk.GUI
 {
@@ -13,24 +14,33 @@ namespace SimpleTalk.GUI
     int _hSpace = 4;
     int _vSpace = 4;
 
+     List<ButtonDefinition> _NewButtons = new List<ButtonDefinition>();
+
+  
+
+     private Font _defaultFont = new Font("Consolas", 45.00F, FontStyle.Bold);
+
     public override void ConstructLayout()
     {
     }
 
     public void ClearButtons()
     {
-      Keyboard.ClearButtons();
+       Keyboard.ClearButtons();
+       _NewButtons.Clear();
     }
 
     public void AddButton(ButtonDefinition button)
     {
-      RowButtons Row = Keyboard.AddRow();
-      Row.AddButton(button.Text, button.Keys, new Size(button.Text.Length * 30 + 70, _DefaultHeight));
+      Graphics dc = Keyboard.GetOwnerControl.CreateGraphics();
+       _NewButtons.Add(new ButtonDefinition(button.Text, button.Keys, new Size((int) dc.MeasureString(button.Text, _defaultFont).Width + 20, _DefaultHeight)));
     }
 
     public void AddButtons(List<String> strings, int max)
     {
       int Number = 0;
+      _NewButtons.Clear();
+
       foreach (string item in strings)
       {
         if (Number >= max)
@@ -42,6 +52,7 @@ namespace SimpleTalk.GUI
           if (item.Length <= maxWordLength)
           {
             AddButton(new ButtonDefinition(item.ToUpper(), "#" + item));
+            
             Number++;
           }
         /*  else
@@ -52,12 +63,39 @@ namespace SimpleTalk.GUI
         }
       }
 
+      NewRow(_DefaultWidth, _DefaultHeight, _NewButtons);
+
       AutoFormat();
     }
 
     public override void AutoFormat()
     {
       Keyboard.AutoFormat(0, 0, _vSpace, _hSpace);
+    }
+
+
+    private void NewRow(int DefaultWidth, int DefaultHeight, IEnumerable<ButtonDefinition> NewButtons)
+    {
+      RowButtons Row = _Keyboard.AddRow();
+      foreach (ButtonDefinition ButtonDef in NewButtons)
+      {
+        if (ButtonDef == null)
+        {
+          Row = _Keyboard.AddRow();
+          continue;
+        }
+        else
+        {
+
+          Row.AddButton(
+              ButtonDef.Text,
+              ButtonDef.Keys,
+              ButtonDef.Size.IsEmpty ? new Size(DefaultWidth, DefaultHeight) : ButtonDef.Size,
+              ButtonDef.AutoRescan,
+              ButtonDef.Skip,
+              ButtonDef.Font);
+        }
+      }
     }
   }
 }
